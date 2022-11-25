53987c8b379f2a0cd61682c96e9bf6bab654811e
rosenkolev1 <50500415+rosenkolev1@users.noreply.github.com>
2022-11-25 23:54:51 +0200

    Add things

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,19 +1,52 @@
 #include "solution.h"
 
 bool isContains(Node* node, int key) {
-    return false;
+    if (!node) {
+        return false;
+    }
+    if (node->key == key)
+        return true;
+
+    if (key < node->key)
+        return isContains(node->left, key);
+
+    return isContains(node->right, key);
 }
 
 Node* insert(Node* node, int value) {
+
+    if(node == nullptr)
+        return new Node(value);
+
+    if(node->key > value) {
+        node->left = insert(node->left, value);
+    }
+    else {
+        node->right = insert(node->right, value);
+    }
+
     return node;
 }
 
 Node* getMax(Node* node) {
-    return nullptr;
+
+    if(!node)
+        return nullptr;
+
+    if(!node->right)
+        return node;
+
+    return getMax(node->right);
 }
 
 Node* getMin(Node* node) {
-    return nullptr;
+    if(!node)
+        return nullptr;
+
+    if(!node->left)
+        return node;
+
+    return getMin(node->left);
 }
 
 Node* remove(Node* node, int key) {
